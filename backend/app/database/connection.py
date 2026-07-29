727c3d404ae703aa95414aa03e713ffa242c75c8
iqrajaved61 <iqramalik1614@gmail.com>
2026-07-30 02:55:35 +0500

    Update connection.py

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
-   from sqlalchemy.orm import sessionmaker
-   from app.core.config import settings
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
 
-   engine = create_engine(settings.DATABASE_URL)
-   SessionLocal = sessionmaker(
-       autocommit=False,
-       autoflush=False,
-       bind=engine
-   )
+engine = create_engine(settings.DATABASE_URL)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
