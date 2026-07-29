0a1f15dc563b58ca93812f117c56966cd6df9a2b
iqrajaved61 <iqramalik1614@gmail.com>
2026-07-30 02:58:21 +0500

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings
 
-   class Settings(BaseSettings):
-       DATABASE_URL: str
+class Settings(BaseSettings):
+    DATABASE_URL: str
 
-       class Config:
-           env_file = ".env"
+    class Config:
+        env_file = ".env"
 
-   settings = Settings()
+settings = Settings()
