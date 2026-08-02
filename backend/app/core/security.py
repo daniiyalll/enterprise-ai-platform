c89d7a5677d224429e0deb22dd461f650fce3bfe
daniiyalll <daniyalgiant1234@gmail.com>
2026-08-02 23:34:42 +0500

    return user role

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,11 +1,16 @@
 import bcrypt
 from datetime import datetime, timedelta
+
 from jose import jwt, JWTError
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
+from sqlalchemy.orm import Session
+
 from app.core.config import settings
+from app.database.session import get_db
+from app.models.user import User
 
 
 SECRET_KEY = settings.SECRET_KEY
@@ -26,6 +31,7 @@ def hash_password(password):
     ).decode("utf-8")
 
 
+
 def verify_password(
     plain_password,
     hashed_password
@@ -35,6 +41,7 @@ def verify_password(
         plain_password.encode("utf-8"),
         hashed_password.encode("utf-8")
     )
+
 
 
 def create_access_token(data):
@@ -58,6 +65,7 @@ def create_access_token(data):
     )
 
 
+
 def decode_access_token(token):
 
     try:
@@ -70,16 +78,20 @@ def decode_access_token(token):
 
         return payload
 
+
     except JWTError:
 
         return None
 
 
+
 def get_current_user(
-    token: str = Depends(oauth2_scheme)
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
 ):
 
     payload = decode_access_token(token)
+
 
     if payload is None:
 
@@ -88,4 +100,29 @@ def get_current_user(
             detail="Invalid or expired token"
         )
 
-    return payload
+
+    username = payload.get("sub")
+
+
+    if username is None:
+
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token"
+        )
+
+
+    user = db.query(User).filter(
+        User.username == username
+    ).first()
+
+
+    if user is None:
+
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+
+    return user
