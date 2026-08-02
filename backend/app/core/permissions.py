bef91a414bf02c0612fb8f9631860f63ef6de8ff
daniiyalll <daniyalgiant1234@gmail.com>
2026-08-02 23:36:46 +0500

    permission

---
+++ backend/app/core/permissions.py
@@ -0,0 +1,65 @@
+from fastapi import Depends, HTTPException
+
+from app.core.security import get_current_user
+from app.models.user import User
+
+
+
+def require_logged_in(
+    current_user: User = Depends(get_current_user)
+):
+
+    return current_user
+
+
+
+def require_admin(
+    current_user: User = Depends(get_current_user)
+):
+
+    if current_user.role != "admin":
+
+        raise HTTPException(
+            status_code=403,
+            detail="Admin access required"
+        )
+
+    return current_user
+
+
+
+def require_manager(
+    current_user: User = Depends(get_current_user)
+):
+
+    if current_user.role not in [
+        "admin",
+        "manager"
+    ]:
+
+        raise HTTPException(
+            status_code=403,
+            detail="Manager access required"
+        )
+
+    return current_user
+
+
+
+def require_employee(
+    current_user: User = Depends(get_current_user)
+):
+
+    if current_user.role not in [
+        "admin",
+        "manager",
+        "employee",
+        "user"
+    ]:
+
+        raise HTTPException(
+            status_code=403,
+            detail="Employee access required"
+        )
+
+    return current_user
